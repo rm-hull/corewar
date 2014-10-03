d50d882f50b83591b2948d9b7039ee66ef6b4f61
Richard Hull <rm_hull@yahoo.co.uk>
2014-10-04 00:13:30 +0100

    Fix bug when s < prefix

--- src/corewar/compat.clj
+++ src/corewar/compat.clj
@@ -6,7 +6,7 @@
     ))
 
 (defn starts-with [s prefix]
-  (let [slice (subs s 0 (count prefix))]
+  (let [slice (subs s 0 (min (count s) (count prefix)))]
     (= slice prefix)))
 
 (defn parse-int [x]
